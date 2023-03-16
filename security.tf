b4ff237aea3415f42d384351e4dded3dfcbb2bac
latesh-11 <121330785+latesh-11@users.noreply.github.com>
2023-03-17 02:03:04 +0530

    Create security.tf

---
+++ security.tf
@@ -0,0 +1,30 @@
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+
+
+ dynamic "ingress" {
+   for_each = var.port
+   iterator = port
+    content {
+    description      = "TLS from VPC"
+    from_port        = port.value
+    to_port          = port.value
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+    }
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}

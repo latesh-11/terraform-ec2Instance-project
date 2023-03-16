5011e566192345df93f36d8def462d0d32d06ad2
latesh-11 <121330785+latesh-11@users.noreply.github.com>
2023-03-17 02:05:38 +0530

    Create aws-ami.tf

---
+++ aws-ami.tf
@@ -0,0 +1,18 @@
+data "aws_ami" "ubuntu" {
+
+  most_recent = true
+  owners      = ["099720109477"]
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-20230208"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
